e2c48560df7b36c7045d9c8a06351c4b9331a9e8
Ross McWey <rossmcwey@gmail.com>
2024-03-26 02:29:54 -0400

    updates to layout, learning next.js, update models

--- pro-finish/src/app/layout.tsx
+++ pro-finish/src/app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <nav>Nav 1</nav>
+      <nav>Nav 2</nav>
+        {children}
+      </body>
     </html>
   );
 }
